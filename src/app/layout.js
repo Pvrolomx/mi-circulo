33c1ecdc635152955b3ad7ceaf6a1e1adbb61cc2
Pvrolomx <pvrolo@gmail.com>
2026-03-29 17:14:53 -0600

    feat: apple-touch-icon butterfly

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="es">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
